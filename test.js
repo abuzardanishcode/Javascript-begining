f5829348019107461decc3a215974c9e845bfe27
Mohammad Abuzar <mohammad.abuzar@unthinkable.co>
2024-05-12 14:23:42 +0530

    variable explaination

---
+++ test.js
@@ -0,0 +1,17 @@
+const Id = 453
+var userName = 'mohammad abuzar';
+let userEmail = "abuzar@gmail.com";
+
+userCity = "Delhi"
+
+
+userName = "dk"
+userEmail ="dk@gmail.com"
+userCity = "khalilabad"
+
+
+//console.table[Id,userEmail, userName, userCity];
+console.log(Id)
+console.log(userName)
+console.log(userEmail)
+console.log(userCity)
